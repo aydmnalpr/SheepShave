1c48e7d082e0097c751b87ec356a5aeeaa4e4307
Alper Ayduman <aydumanalper@gmail.com>
2020-12-02 23:34:29 +0300

    Initial commit

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+    bool mouseClickjudge;
+    public Transform target;
+    public Camera cam;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //CameraController();
+    }
+
+    public void CameraController()
+    {
+        if (Input.GetMouseButtonDown(0)) {
+            mouseClickjudge = true;
+        }
+
+        if (Input.GetMouseButtonUp(0)) {
+            mouseClickjudge = false;
+        }
+        if (mouseClickjudge) {
+            cam.transform.RotateAround(target.position, Vector3.up, Input.GetAxis("Mouse X") * 3);
+        }
+    }
+}
---
+++ Assets/Scripts/Shaver.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shaver : MonoBehaviour
+{
+    public ParticleSystem particle;
+    private Vector3 offset;
+    private float zCoord;
+    Camera cam;
+    Transform top;
+
+    private void Awake()
+    {
+        cam = Camera.main;
+        top = GetComponentInChildren<Transform>();
+    }
+
+    private void OnMouseDown()
+    {
+        zCoord = cam.WorldToScreenPoint(transform.position).z;
+        offset = transform.position - GetMouseWorldPos();
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 total = GetMouseWorldPos() + offset;
+        transform.position = new Vector3(transform.position.x, total.y, total.z);
+    }
+
+    private Vector3 GetMouseWorldPos()
+    {
+        Vector3 mousePosition = Input.mousePosition;
+        mousePosition.z = zCoord;
+        return cam.ScreenToWorldPoint(mousePosition);
+    }
+
+    private void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(top.position, -transform.forward, out hit, 100))
+        {
+            particle.Emit(5);
+            //Destroy(hit.transform.gameObject);
+            Rigidbody hitObject = hit.transform.gameObject.GetComponent<Rigidbody>();
+            hitObject.isKinematic = false;
+            int random = UnityEngine.Random.Range(8, 15);
+            hitObject.AddForce(new Vector3(random, random, 0), ForceMode.Impulse);
+            //Handheld.Vibrate();
+
+        }
+
+        Debug.DrawRay(top.position, -transform.forward, Color.green, .01f, false);
+        
+
+    }
+
+
+}
